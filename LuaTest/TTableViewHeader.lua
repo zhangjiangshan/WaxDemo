c37a5d08f0dbeb0b0c03f889226c3317e7a12b04
zjs <zhangjiangshan@meituan.com>
2014-08-01 16:46:57 +0800

    添加header

---
+++ LuaTest/TTableViewHeader.lua
@@ -0,0 +1,12 @@
+
+waxClass{"TTableViewHeader", UIView}
+
+function initWithFrame(self,frame)
+    self.super : initWithFrame(style, frame)
+    self.label = UILabel:initWithFrame(CGRect(0,0,200,20))
+    self.label:setBackgroundColor(UIColor:whitColor())
+    self.label:setTextColor(UIColor:blackColor)
+    self:label():setText("这是远程下载的")
+    self:addSubview(self.label)
+    return self
+end
