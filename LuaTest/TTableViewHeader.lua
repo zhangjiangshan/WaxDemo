028e2307d6f60ce0f0e5b05489e38fb771aab623
zjs <zhangjiangshan@meituan.com>
2014-08-01 18:16:41 +0800

    das

--- LuaTest/TTableViewHeader.lua
+++ LuaTest/TTableViewHeader.lua
@@ -2,7 +2,7 @@
 waxClass{"TTableViewHeader", UIView}
 
 function initWithFrame(self,frame)
-    self.super:initWithFrame(self, frame)
+    self.super:initWithFrame(frame)
     self.label = UILabel:initWithFrame(CGRect(0,0,320,20))
     self.label:setBackgroundColor(UIColor:whiteColor())
     self.label:setTextColor(UIColor:blackColor())
